21e24b2c32a0b0d883e41bc7374126fff025aa94
JorgePinargote <jorge.apif@gmail.com>
2019-01-22 23:44:37 -0500

    Leyendo csv y mostrando datos, avance 3

--- lenguajes.php
+++ lenguajes.php
@@ -14,28 +14,22 @@
         require 'simple_html_dom.php';
         //Codigo del analisis a Computrabajo
         //Hay que poner una variable para el empleo a buscar, aqui por defecto le puse contador. 
-        $html = file_get_html('https://www.computrabajo.com.ec/ofertas-de-trabajo/?q=programador'); 
-
-        $container = $html->find('div[id=MainContainer]',0);
-        $parrilla = $container->find('section[class=parrilla_oferta]',0);
-        $ofertas = $parrilla->find('div[class=gO]',0);
-        $oferta1div = $ofertas->find('div[class=bRS]');
         
-        $nombre_archivo='archivo.csv';
+        $html = file_get_html('https://www.computrabajo.com.ec/ofertas-de-trabajo/?q=programador'); 
+        $oferta1div = $html->find('div[class=bRS]');
+        
+        $nombre_archivo='computrabajo.csv';
         $archivo = fopen($nombre_archivo, "a");
 
         foreach ($oferta1div as $oft) {
             $descripcion = $oft->find('div[class=iO]',0);
             $h2 = $descripcion->find('h2[class=tO]',0);
             $link = $h2->find('a[class=js-o-link]',0);
-            //echo $link->href . "<br>";
             
             //Analisis de la descripcion de los trabajos
             $linkdes = 'https://www.computrabajo.com.ec'.$link->href;
             $htmldes = file_get_html($linkdes);
-            $containerdes = $htmldes->find('div[id=MainContainer]',0);
-            $articulo = $containerdes->find('article[class=fl]',0);
-            $seccion = $articulo->find('section[class=box_r]',0);
+            $seccion = $htmldes->find('section[class=box_r]',0);
 
             $ul = $seccion->find('ul li p');
 
@@ -46,19 +40,57 @@
             $tipo =  $ul[4];
             $salario = $ul[5];
 
-            fwrite($archivo, $trabajo->plaintext .','.$empresa->plaintext.
-            ','.$localizacion->plaintext .','. $jornada->plaintext.','.$tipo->plaintext.','.$salario->plaintext);
+            $trpro = str_replace(",",";",trim($trabajo->plaintext)); //cadena de trabajo, procesado. 
+            $empro =  str_replace(",",";",trim($empresa->plaintext));
+            $locpro = str_replace(",",";",trim($localizacion->plaintext));
+            $jorpro = str_replace(",",";",trim($jornada->plaintext));
+            $tipro = str_replace(",",";",trim($tipo->plaintext)); 
+            $salpro = str_replace(",",";",trim($salario->plaintext));
+            
+            if(count($ul) == 6){
+                fwrite($archivo,  $trpro.','.$empro.
+                ','.$locpro.','.$jorpro .','. $tipro .','.$salpro."\r\n");
+            }
 
-            echo $trabajo->outertext;
-            echo $empresa->plaintext;
-            echo $localizacion->outertext;
-            echo $jornada->outertext;
-            echo $tipo->outertext;
-            echo $salario->outertext;
+        }
+        
+        fclose($archivo);
+        
+        //Codigo del analisis a Multitrabajo
+        $htmlmul = file_get_html('https://www.multitrabajos.com/empleos-busqueda-programador.html'); 
+        $ofertasmul = $htmlmul->find('div[class=aviso]');
 
-        }           
+        $nombre_archivo1='multitrabajo.csv';
+        $archivo1 = fopen($nombre_archivo1, "a");
 
-       fclose($archivo);
+        foreach ($ofertasmul as $ofm) {
+           $descmul = file_get_html('https://www.multitrabajos.com'.$ofm->id); 
+           $header = $descmul->find('div[class=aviso_header]',0);
+           $title = $header->find('h1[class=aviso_title]',0);
+           $enterprise = $header->find('a h2',0);
+           $specs = $descmul->find('div[class=aviso_specs]',0);
+           $data = $specs->find('div[class=z-group]');
+
+           $lugar = $data[0]->children(1);
+           $tipo = $data[3]->children(1);
+           $paga =  $data[2]->children(1);
+
+
+           $titlepro = str_replace(",",";",trim($title->plaintext)); //cadena de trabajo, procesado. 
+
+           $emp = $enterprise->plaintext;
+           if($enterprise==NULL) $emp = "Confidencial";
+
+           $entpro =  str_replace(",",";",trim($emp));
+           $lugarpro = str_replace(",",";",trim($lugar->plaintext));
+           $tipopro = str_replace(",",";",trim($tipo->plaintext));
+           $salariopro = str_replace(",",";",trim($paga->plaintext)); 
+
+           fwrite($archivo1,  $titlepro.','.$entpro.','.$lugarpro .','. $tipopro .','.$salariopro."\r\n");
+           
+        }
+
+        fclose($archivo1);
 
     ?>
 
